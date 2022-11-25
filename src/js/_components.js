fe35dad96e00d108f1b6a9975eb0089e8ab1e752
Vitaly Reutsky <vreutsky77@mail.ru>
2022-11-25 18:39:03 +0300

    Десктопная версия готова

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import "./components/smooth-scroll";
 import "./components/burger";
 import "./components/gallery";
 import "./components/faq";
+import "./components/dynamic-adapt";
--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -7,15 +7,18 @@ import { enableScroll } from "../functions/enable-scroll";
   const menu = document?.querySelector("[data-menu]");
   const menuItems = document?.querySelectorAll("[data-menu-item]");
   const overlay = document?.querySelector("[data-menu-overlay]");
+  const headerFilter = document.querySelector(".header");
 
   burger?.addEventListener("click", (e) => {
     burger?.classList.toggle("burger--active");
     menu?.classList.toggle("menu--active");
+    headerFilter?.classList.toggle("hide-backdrop-filter");
 
     if (menu?.classList.contains("menu--active")) {
       navMenu.classList.add("mob-menu--show");
       burger?.setAttribute("aria-expanded", "true");
       burger?.setAttribute("aria-label", "Закрыть меню");
+
       disableScroll();
     } else {
       navMenu.classList.remove("mob-menu--show");
@@ -39,6 +42,8 @@ import { enableScroll } from "../functions/enable-scroll";
       burger?.setAttribute("aria-label", "Открыть меню");
       burger.classList.remove("burger--active");
       menu.classList.remove("menu--active");
+      navMenu.classList.remove("mob-menu--show");
+      headerFilter?.classList.remove("hide-backdrop-filter");
       enableScroll();
     });
   });
