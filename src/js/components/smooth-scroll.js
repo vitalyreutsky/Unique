f63ae7488f6c9410819f890d58c0a23040ffff27
Vitaly Reutsky <vreutsky77@mail.ru>
2022-11-26 16:06:34 +0300

    Адаптировал страницу под 375px

--- src/js/components/smooth-scroll.js
+++ src/js/components/smooth-scroll.js
@@ -1,7 +1,7 @@
 // Подключение плавной прокрутки к якорям
 import SmoothScroll from "smooth-scroll";
 const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
+  speed: 700,
   speedAsDuration: true,
   header: "[data-scroll-header]",
 });
